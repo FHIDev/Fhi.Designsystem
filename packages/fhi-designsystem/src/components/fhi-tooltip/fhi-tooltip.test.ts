57c5545054b42ba96304bb1ecea37008ad5a25eb
Daniel Nilsen <dafn@Daniels-MBP.home>
2025-05-28 16:31:59 +0200

    add more tests

--- packages/fhi-designsystem/src/components/fhi-tooltip/fhi-tooltip.test.ts
+++ packages/fhi-designsystem/src/components/fhi-tooltip/fhi-tooltip.test.ts
@@ -67,40 +67,181 @@ describe('fhi-tooltip', () => {
 
       expect(tooltip?.getAttribute('aria-hidden')).to.equal('false');
     });
-
-    it('has aria-hidden set to "false" when visible', async () => {
-      const tooltip = component.shadowRoot?.querySelector('#tooltip');
-
-      expect(tooltip?.getAttribute('aria-hidden')).to.equal('true');
-    });
   });
 
-  /*
-
-  describe('interactions', () => {
+  describe('setting attributes', () => {
     beforeEach(async () => {
       component = await fixture(
         html`<fhi-tooltip message="myTooltip">
-          <span>My Element</span>
+          <span id="child">My Element</span>
         </fhi-tooltip>`,
       );
     });
 
-    it('will open, and become accessable, on child click', () => {
-      const child = component.querySelector('span');
-      child?.dispatchEvent(new Event('click'));
+    it('has an attribute to set the attribute', async () => {
+      component = await fixture(
+        html`<fhi-tooltip message="myTooltip">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
 
-      console.log(component.shadowRoot?.querySelector('.tooltip'));
+      expect(component.getAttribute('message')).to.equal('myTooltip');
+      expect(component.message).to.equal('myTooltip');
+    });
 
-      expect(
-        component.shadowRoot?.querySelector('.tooltip'),
-      ).to.be.accessible();
+    it('has an attribute to set the trigger', async () => {
+      component = await fixture(
+        html`<fhi-tooltip trigger="click">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
 
-      expect(
-        component.shadowRoot?.querySelector('.tooltip'),
-      ).not.to.be.accessible();
+      expect(component.getAttribute('trigger')).to.equal('click');
+      expect(component.trigger).to.equal('click');
+    });
+
+    it('has an attribute to set the placement', async () => {
+      component = await fixture(
+        html`<fhi-tooltip placement="bottom">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      expect(component.getAttribute('placement')).to.equal('bottom');
+      expect(component.placement).to.equal('bottom');
+    });
+
+    it('has an attribute to set the delay', async () => {
+      component = await fixture(
+        html`<fhi-tooltip delay="200">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      expect(component.getAttribute('delay')).to.equal('200');
+      expect(component.delay).to.equal('200');
+    });
+
+    it('has an attribute to set the maxWidth', async () => {
+      component = await fixture(
+        html`<fhi-tooltip max-width="100px">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      expect(component.getAttribute('max-width')).to.equal('100px');
+      expect(component.maxWidth).to.equal('100px');
     });
   });
 
-  */
+  describe('form association', () => {
+    it('does not interfer with form association', async () => {
+      component = await fixture(
+        html`<fhi-tooltip message="myTooltip"
+          ><input name="myInput"
+        /></fhi-tooltip>`,
+        { parentNode: document.createElement('form') },
+      );
+
+      const form = new FormData(
+        document.querySelector('form') as HTMLFormElement,
+      );
+
+      expect(form.get('myInput')).to.not.equal(null);
+      expect(form.get('myInput')).to.not.equal(undefined);
+    });
+  });
+
+  describe('interactions', () => {
+    it('will open on child hover', async () => {
+      component = await fixture(
+        html`<fhi-tooltip message="myTooltip">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      const anchor = component.shadowRoot?.querySelector(
+        '#tooltip-anchor',
+      ) as HTMLElement;
+      const tooltip = component.shadowRoot?.querySelector('#tooltip');
+
+      anchor.dispatchEvent(new Event('mouseenter'));
+
+      // Wait for the tooltip open delay and animation.
+      await new Promise(resolve => setTimeout(resolve, 600));
+
+      const istooltipVisible = tooltip?.checkVisibility({
+        checkOpacity: true,
+        checkVisibilityCSS: true,
+      });
+
+      expect(istooltipVisible).to.equal(true);
+      expect(tooltip?.getAttribute('aria-hidden')).to.equal('false');
+    });
+
+    it('will close on child hover exit', async () => {
+      component = await fixture(
+        html`<fhi-tooltip message="myTooltip">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      const anchor = component.shadowRoot?.querySelector(
+        '#tooltip-anchor',
+      ) as HTMLElement;
+
+      const tooltip = component.shadowRoot?.querySelector('#tooltip');
+
+      anchor.dispatchEvent(new Event('mouseenter'));
+
+      // Wait for the tooltip open delay and animation.
+      await new Promise(resolve => setTimeout(resolve, 600));
+
+      const istooltipVisible = tooltip?.checkVisibility({
+        checkOpacity: true,
+        checkVisibilityCSS: true,
+      });
+
+      expect(istooltipVisible).to.equal(true);
+
+      anchor.dispatchEvent(new Event('mouseleave'));
+
+      // Wait for the tooltip close animation.
+      await new Promise(resolve => setTimeout(resolve, 150));
+
+      const isTooltipHidden = tooltip?.checkVisibility({
+        checkOpacity: true,
+        checkVisibilityCSS: true,
+      });
+
+      expect(isTooltipHidden).to.equal(false);
+      expect(tooltip?.getAttribute('aria-hidden')).to.equal('true');
+    });
+
+    it('will open on child click', async () => {
+      component = await fixture(
+        html`<fhi-tooltip message="myTooltip" trigger="click">
+          <span id="child">My Element</span>
+        </fhi-tooltip>`,
+      );
+
+      const tooltip = component.shadowRoot?.querySelector('#tooltip');
+      const child = component.querySelector('#child') as HTMLElement;
+
+      child?.click();
+
+      // Wait for the tooltip animation to finish
+      await new Promise(resolve => setTimeout(resolve, 100));
+
+      await component.updateComplete;
+
+      const istooltipVisible = tooltip?.checkVisibility({
+        checkOpacity: true,
+        checkVisibilityCSS: true,
+      });
+
+      expect(istooltipVisible).to.equal(true);
+      expect(tooltip?.getAttribute('aria-hidden')).to.equal('false');
+    });
+  });
 });
