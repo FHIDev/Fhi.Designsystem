5680c4299c89f0b2f85cceb67ebe47a9fd2f543f
Daniel Nilsen <dafn@Daniels-MBP.home>
2025-07-29 15:04:56 +0200

    add styling for default radio

--- packages/fhi-designsystem/src/components/fhi-radio/fhi-radio.component.ts
+++ packages/fhi-designsystem/src/components/fhi-radio/fhi-radio.component.ts
@@ -11,6 +11,10 @@ export class FhiRadio extends LitElement {
 
   @property({ type: String }) name?: string = undefined;
 
+  @property({ type: String, reflect: true }) status?: 'error' = undefined;
+
+  @property({ type: Boolean, reflect: true }) disabled? = false;
+
   @property({ type: Boolean, reflect: true }) checked? = false;
 
   @query('#input-element') _input!: HTMLInputElement;
@@ -58,17 +62,103 @@ export class FhiRadio extends LitElement {
     }
 
     return html`
-      <input
-        type="radio"
-        id="input-element"
-        name="${this.name}"
-        .value="${this.name}"
-        ?checked=${this.checked}
-        @change=${this.handleChange}
-      />
+      <div id="radio-container">
+        <input
+          type="radio"
+          id="input-element"
+          name="${this.name}"
+          .value="${this.name}"
+          ?checked=${this.checked}
+          @change=${this.handleChange}
+        />
+        <svg
+          class="radio-dot"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle r="6" cx="9" cy="9" />
+        </svg>
+      </div>
       <label for="input-element">${this.label}</label>
     `;
   }
 
-  static styles = css``;
+  static styles = css`
+    :host {
+      --color-radio-border: var(--fhi-color-neutral-border-default);
+      --color-radio-label: var(--fhi-color-neutral-text-default);
+      --color-radio-background: var(--fhi-color-neutral-background-default);
+
+      --color-radio-border-hover: var(--fhi-color-accent-border-strong);
+      --color-radio-label-hover: var(--fhi-color-accent-border-strong);
+      --color-radio-background-hover: var(--fhi-color-accent-background-subtle);
+
+      --color-radio-outline: var(--fhi-color-accent-surface-default);
+
+      /* Error state */
+      --color-radio-label-error: var(--fhi-color-neutral-text-default);
+      --color-radio-border-error: var(--fhi-color-danger-border-default);
+      --color-radio-background-error: var(
+        --fhi-color-danger-background-default
+      );
+    }
+
+    :host {
+      display: flex;
+      align-items: center;
+      width: max-content;
+      gap: 0.5rem;
+
+      #radio-container {
+        position: relative;
+      }
+
+      input {
+        margin: 0;
+        appearance: none;
+        width: 1.25rem;
+        height: 1.25rem;
+        background-color: var(--color-radio-background);
+        border: 0.0625rem solid var(--color-radio-border);
+        border-radius: 50%;
+      }
+      .radio-dot {
+        visibility: hidden;
+        position: absolute;
+        top: 1px;
+        left: 1px;
+      }
+
+      input:checked {
+        border-width: 2px;
+        ~ .radio-dot {
+          circle {
+            fill: var(--color-radio-border);
+          }
+          visibility: visible;
+        }
+      }
+    }
+
+    :host(:hover) {
+      input:not(:checked) {
+        border-color: var(--color-radio-border-hover);
+        background-color: var(--color-radio-background-hover);
+      }
+    }
+
+    :host(:active) {
+      input:not(:checked) {
+        outline: 0.25rem solid var(--color-radio-outline);
+      }
+    }
+
+    :host[status='error'] {
+    }
+
+    :host([disabled]) {
+    }
+  `;
 }
