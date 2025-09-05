c20bf816e9636fad59395805f44289dacacd8344
HenrikFalla <henrik.falla@fhi.no>
2025-09-05 09:04:38 +0200

    File structure and boilerplate for component.

---
+++ packages/fhi-designsystem/src/components/fhi-flex/fhi-flex.component.ts
@@ -0,0 +1,13 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+export const FhiFlexSelector = 'fhi-flex';
+
+@customElement(FhiFlexSelector)
+export class FhiFlex extends LitElement {
+  render() {
+    return html``;
+  }
+
+  static styles = css``;
+}
