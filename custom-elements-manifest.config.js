7329666d2105c52efb01d56b59612f2077870a8b
HenrikFalla <henrik.falla@fhi.no>
2025-09-11 12:26:46 +0200

    Revert "First attempt at setting up custom-elements-manifest"
    This reverts commit 9ebb79baf73865dc1cd15a319d6dd87b494a0784.

--- custom-elements-manifest.config.js
+++
@@ -1,9 +0,0 @@
-import { litPlugin } from 'cem-plugin-lit-describe';
-
-export default {
-  globs: ['packages/fhi-designsystem/src/components/**/*.ts'],
-  exclude: ['**/*.test.ts', '**/*.stories.ts'],
-  plugins: [litPlugin()],
-  outdir: 'packages/fhi-designsystem/custom-elements.json',
-  litelement: true,
-};
