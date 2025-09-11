9c8f9f55116e1a4a54016920a0c7ce27a2851743
HenrikFalla <henrik.falla@fhi.no>
2025-09-11 12:26:40 +0200

    Revert "Test"
    This reverts commit f2a84c31cd8ed3ef59e8e5b044ad0bc9abaeb4f6.

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,15 +1,9 @@
-import { getTsProgram, typeParserPlugin } from '@wc-toolkit/type-parser';
+import { litPlugin } from 'cem-plugin-lit-describe';
 
 export default {
-  globs: ['src/components/**/*.ts'],
+  globs: ['packages/fhi-designsystem/src/components/**/*.ts'],
   exclude: ['**/*.test.ts', '**/*.stories.ts'],
-  // Give the plugin access to the TypeScript type checker
-  overrideModuleCreation({ ts, globs }) {
-    const program = getTsProgram(ts, globs, 'tsconfig.json');
-    return program
-      .getSourceFiles()
-      .filter(sf => globs.find(glob => sf.fileName.includes(glob)));
-  },
-  plugins: [typeParserPlugin()],
-  outdir: '/dist/npm',
+  plugins: [litPlugin()],
+  outdir: 'packages/fhi-designsystem/custom-elements.json',
+  litelement: true,
 };
