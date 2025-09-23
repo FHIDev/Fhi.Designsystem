a6a98d2f52fd5657f04de04f0b16bdcf8da9d3d2
Marthe Søvdsnes <msso@fhi.no>
2025-09-23 13:41:37 +0200

    Kan endre antall rader og kolonner.

--- packages/fhi-designsystem/src/components/fhi-grid/fhi-grid.component.ts
+++ packages/fhi-designsystem/src/components/fhi-grid/fhi-grid.component.ts
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import { html, css, LitElement, PropertyValues } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 export const FhiGridSelector = 'fhi-grid';
@@ -7,11 +7,39 @@ type FhiGapWidthUnit = `${number}${FhiUnitType}` | number;
 
 @customElement(FhiGridSelector)
 export class FhiGrid extends LitElement {
-  @property({ type: String, reflect: true }) gap:
+  @property({ type: String }) gap:
     | 'small'
     | 'medium'
     | 'large'
     | FhiGapWidthUnit = 'medium';
+  @property({ type: Number }) rows?: number;
+  @property({ type: Number }) columns = 12;
+
+  updated(changedProperties: PropertyValues<this>) {
+    super.updated(changedProperties);
+
+    if (changedProperties.has('rows')) {
+      this.style.gridTemplateRows = `repeat( ${this.rows}, 1fr )`;
+    }
+
+    if (changedProperties.has('columns')) {
+      this.style.gridTemplateColumns = `repeat( ${this.columns}, 1fr )`;
+    }
+
+    if (changedProperties.has('gap')) {
+      const isPresetGap = ['small', 'medium', 'large'].includes(
+        String(this.gap),
+      );
+
+      if (isPresetGap) {
+        this.style.gap = '';
+        return;
+      }
+      const gapValue = Number(this.gap) ? `${this.gap}px` : this.gap;
+      this.style.gap = gapValue as string;
+    }
+  }
+
   render() {
     return html`<slot></slot>`;
   }
@@ -19,12 +47,12 @@ export class FhiGrid extends LitElement {
   static styles = css`
     :host {
       --dimension-grid-gap-small: var(--fhi-spacing-100);
-      --dimesnion-grid-gap-medium: var(--fhi-spacing-200);
+      --dimension-grid-gap-medium: var(--fhi-spacing-200);
       --dimension-grid-gap-large: var(--fhi-spacing-300);
     }
     :host {
       display: grid;
-      gap: var(--dimesnion-grid-gap-medium);
+      gap: var(--dimension-grid-gap-medium);
     }
     :host([gap='small']) {
       gap: var(--dimension-grid-gap-small);
--- packages/fhi-designsystem/src/components/fhi-grid/fhi-grid.stories.ts
+++ packages/fhi-designsystem/src/components/fhi-grid/fhi-grid.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { FhiGrid } from './fhi-grid.component';
 import { FhiButton } from '../fhi-button/fhi-button.component';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 new FhiGrid();
 new FhiButton();
@@ -21,10 +22,19 @@ const meta: Meta<FhiGrid> = {
   render: args => html`<fhi-grid gap=${args.gap}></fhi-grid>`,
   argTypes: {
     gap: {
-      control: { type: 'text' },
+      control: 'text',
       description:
-        'Bestemmer avstand mellom elementene. Kan være <code>small</code>, <code>medium</code>, <code>large</code> eller en spesifikk verdi som <code>10px</code>, <code>1rem</code> eller <code>20<code>.',
+        'Bestemmer avstand mellom elementene. Kan være `small`, `medium`, `large` eller en spesifikk verdi som `10px`, `1rem` eller `20`.',
       defaultValue: { summary: 'medium' },
+    },
+    rows: {
+      controls: 'number',
+      description: 'Bestemmer antall rader.',
+    },
+    columns: {
+      controls: 'number',
+      description: 'Bestemmer antall kolonner',
+      defaultValue: { summary: '12' },
     },
   },
 };
@@ -35,13 +45,31 @@ export const Preview: Story = {
   tags: [],
   args: {
     gap: 'medium',
+    rows: 3,
+    columns: 10,
   },
   render: args => html`
-    <fhi-flex gap=${args.gap}>
-      <fhi-button variant="strong">Knapp</fhi-button>
-      <fhi-button variant="subtle">Knapp</fhi-button>
-      <fhi-button variant="text">Knapp</fhi-button>
-    </fhi-flex>
+    <fhi-grid
+      gap=${args.gap}
+      rows=${ifDefined(args.rows)}
+      columns=${args.columns}
+    >
+      <div
+        style="grid-column: span 4;  grid-row: span 2; font-family: var(--fhi-font-family-default); color: var(--fhi-color-accent-text-subtle); background: var(--fhi-color-accent-background-subtle); border: var(--fhi-dimension-border-width) solid var(--fhi-color-accent-border-subtle); padding: var(--fhi-spacing-200); border-radius: var(--fhi-border-radius-150); display: flex; justify-content: start; align-items: center;"
+      >
+        Slot
+      </div>
+      <div
+        style="grid-column: span 6;  font-family: var(--fhi-font-family-default); color: var(--fhi-color-accent-text-subtle); background: var(--fhi-color-accent-background-subtle); border: var(--fhi-dimension-border-width) solid var(--fhi-color-accent-border-subtle); padding: var(--fhi-spacing-200); border-radius: var(--fhi-border-radius-150); display: flex; justify-content: start; align-items: center;"
+      >
+        Slot
+      </div>
+      <div
+        style="grid-column: span 6;  font-family: var(--fhi-font-family-default); color: var(--fhi-color-accent-text-subtle); background: var(--fhi-color-accent-background-subtle); border: var(--fhi-dimension-border-width) solid var(--fhi-color-accent-border-subtle); padding: var(--fhi-spacing-200); border-radius: var(--fhi-border-radius-150); display: flex; justify-content: start; align-items: center;"
+      >
+        Slot
+      </div>
+    </fhi-grid>
   `,
 };
 
