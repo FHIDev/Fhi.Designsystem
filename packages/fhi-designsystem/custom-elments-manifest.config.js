f6f682e3884fabc37b489c4c0a7f964b98537fee
HenrikFalla <henrik.falla@fhi.no>
2025-10-03 13:25:04 +0200

    Revert "Manifest file update"
    This reverts commit 30b7683c51711c28c2a279d676ed10ed43444543.

--- packages/fhi-designsystem/custom-elments-manifest.config.js
+++ packages/fhi-designsystem/custom-elments-manifest.config.js
@@ -4,7 +4,7 @@ import { typeParserPlugin } from '@wc-toolkit/type-parser';
 export default {
   globs: ['src/components/**/*.component.ts'],
   exclude: ['**/*.test.ts', '**/*.stories.ts'],
-  outdir: 'dist/npm/',
+  outdir: './',
   LitElement: true,
   plugins: [
     cemSorterPlugin({
