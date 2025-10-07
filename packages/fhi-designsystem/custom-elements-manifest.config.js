b56083b06afcf5b15a9dbe69cfff23d2f3720864
HenrikFalla <henrik.falla@fhi.no>
2025-10-07 12:02:23 +0200

    Update custom-elements-manifest.config.js
    Added options for JetBrains plugin

--- packages/fhi-designsystem/custom-elements-manifest.config.js
+++ packages/fhi-designsystem/custom-elements-manifest.config.js
@@ -1,6 +1,27 @@
 import { cemSorterPlugin } from '@wc-toolkit/cem-sorter';
 import { typeParserPlugin } from '@wc-toolkit/type-parser';
 
+const options = {
+  outdir: './',
+  webTypesFileName: 'web-types.json',
+  descriptionSrc: 'description',
+  hideSlotDocs: false,
+  hideEventDocs: false,
+  hideCssPropertiesDocs: false,
+  hideCssPartsDocs: false,
+  hideMethodDocs: true,
+  excludeHtml: false,
+  excludeCss: true,
+  labels: {
+    slots: 'Slot Section',
+    events: 'Custom Events',
+    cssProperties: 'CSS Variables',
+    cssParts: 'Style Hooks',
+    methods: 'Methods',
+  },
+  typesSrc: 'expandedType',
+};
+
 export default {
   globs: ['src/components/**/*.component.ts'],
   exclude: ['**/*.test.ts', '**/*.stories.ts'],
@@ -12,5 +33,6 @@ export default {
       customFields: ['customProperty'],
     }),
     typeParserPlugin({ propertyName: 'expandedType' }),
+    customElementJetBrainsPlugin(options),
   ],
 };
