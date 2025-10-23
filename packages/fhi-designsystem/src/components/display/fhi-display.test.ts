2eba70bdc87a17363b24cdb02cfb9f778cb67845
Marthe Søvdsnes <msso@fhi.no>
2025-10-23 14:59:44 +0200

    Attributt-tester.

--- packages/fhi-designsystem/src/components/display/fhi-display.test.ts
+++ packages/fhi-designsystem/src/components/display/fhi-display.test.ts
@@ -25,4 +25,44 @@ describe('fhi-display', () => {
       expect(component.hasAttribute('role')).to.equal(false);
     });
   });
+
+  describe('Setting attributes', () => {
+    it('has an attribute to set level', async () => {
+      const component = await fixture<FhiDisplay>(
+        html`<fhi-display level="1">Test</fhi-display>`,
+      );
+
+      expect(component.getAttribute('level')).to.equal('1');
+      expect(component.level).to.equal(1);
+    });
+
+    it('has an attribute to set color', async () => {
+      const component = await fixture<FhiDisplay>(
+        html`<fhi-display level="1" color="black">Test</fhi-display>`,
+      );
+
+      expect(component.getAttribute('color')).to.equal('black');
+      expect(component.color).to.equal('black');
+    });
+
+    it('has an attribute to set size', async () => {
+      const component = await fixture<FhiDisplay>(
+        html`<fhi-display level="1" size="small">Test</fhi-display>`,
+      );
+
+      expect(component.getAttribute('size')).to.equal('small');
+      expect(component.size).to.equal('small');
+    });
+  });
+
+  it('renders the correct h level', async () => {
+    component = await fixture(html`
+      <fhi-display level="2">Test</fhi-display>
+    `);
+    expect(component).shadowDom.to.equal(`
+      <h2 class="display">
+        <slot></slot>
+      </h2>
+    `);
+  });
 });
