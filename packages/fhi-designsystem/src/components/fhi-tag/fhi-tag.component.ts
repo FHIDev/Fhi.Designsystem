7560fbe7090de23e05c9423cfd0783a98655183f
Marthe Søvdsnes <msso@fhi.no>
2025-11-10 14:47:25 +0100

    Innhold i filene.

--- packages/fhi-designsystem/src/components/fhi-tag/fhi-tag.component.ts
+++ packages/fhi-designsystem/src/components/fhi-tag/fhi-tag.component.ts
@@ -5,7 +5,12 @@ export const FhiTagSelector = 'fhi-tag';
 
 @customElement(FhiTagSelector)
 export class FhiTag extends LitElement {
-  @property({ type: String }) variant:
+  /**
+   * Sets the color theme of the tag.
+   * @attr
+   * @type {'accent' | 'neutral' | 'danger'}
+   */
+  @property({ type: String, reflect: true }) color:
     | 'neutral'
     | 'accent'
     | 'success'
@@ -13,40 +18,105 @@ export class FhiTag extends LitElement {
     | 'danger'
     | 'info' = 'neutral';
 
-  render() {
-    return html`
-      <div>
-        <slot></slot>
-      </div>
-    `;
-  }
-  static styles = css`
-    :host {
-      display: flex;
-      padding: 0 var(--fhi-spacing-050);
-      justify-content: center;
-      align-items: center;
-      width: fit-content;
-      border-radius: var(--fhi-border-radius-050);
-      border-width: var(--fhi-dimension-border-width-focus);
-      background: var(--fhi-color-danger-surface-default);
+  private _handleSlotChange(event: Event): void {
+    const nodes = (event.target as HTMLSlotElement).assignedNodes();
 
-      color: var(--fhi-color-danger-text-default);
-      background-color: var(--fhi-color-danger-surface-default);
-      text-align: justify;
-      font-variant-numeric: lining-nums proportional-nums;
+    const validNodes = nodes.filter(
+      node =>
+        node.nodeType === Node.ELEMENT_NODE ||
+        (node.nodeType === Node.TEXT_NODE && node.textContent?.trim()),
+    );
 
-      font-family: var(--fhi-font-family-default);
-      font-size: var(--fhi-typography-body-small-font-size);
-      font-style: normal;
-      font-weight: 400;
-      line-height: var(--fhi-typography-body-small-line-height);
-      letter-spacing: var(--fhi-typography-body-small-letter-spacing);
+    if (validNodes.length === 0) {
+      return;
     }
 
-    :host[variant='accent'] {
-      color: var(--fhi-color-accent-surface-default);
-      background-color: var(--fhi-color-accent-text-default);
+    for (const node of validNodes) {
+      if (
+        node.nodeType === Node.ELEMENT_NODE &&
+        (node as Element).tagName.toLowerCase().startsWith('fhi-icon')
+      ) {
+        const icon = node as HTMLElement;
+
+        icon.setAttribute('size', '1rem');
+        icon.style.marginLeft = 'var(--dimension-icon-offset)';
+      }
+    }
+  }
+
+  render() {
+    return html` <slot @slotchange=${this._handleSlotChange}></slot> `;
+  }
+
+  static styles = css`
+    :host {
+      --typography-font-family: var(--fhi-font-family-default);
+      --typography-font-size: var(--fhi-typography-body-small-font-size);
+      --typography-font-weight: var(--fhi-typography-body-small-font-weight);
+      --typography-font-height: var(--fhi-typography-body-small-line-height);
+      --typography-letter-spacing: var(
+        --fhi-typography-body-small-letter-spacing
+      );
+      --typography-font-variant-numeric: lining-nums proportional-nums;
+
+      --dimension-icon-offset: calc(-1 * var(--fhi-spacing-050));
+      --dimension-border-radius: var(--fhi-border-radius-050);
+      --dimension-gap: var(--fhi-spacing-050);
+      --dimension-padding: 0 var(--fhi-spacing-100);
+      --dimension-height: var(--fhi-spacing-300);
+
+      --color-neutral-text: var(--fhi-color-neutral-text-default);
+      color: var(--fhi-color);
+      background-color: var(--fhi-background-color);
+    }
+
+    :host {
+      display: flex;
+      width: fit-content;
+      align-items: center;
+      justify-content: center;
+
+      font-family: var(--typography-font-family);
+      font-size: var(--typography-font-size);
+      font-weight: var(--typography-font-weight);
+      line-height: var(--typography-font-height);
+      letter-spacing: var(--typography-letter-spacing);
+      font-variant-numeric: var(--typography-font-variant-numeric);
+
+      border-radius: var(--dimension-border-radius);
+      gap: var(--dimension-gap);
+      padding: var(--dimension-padding);
+      height: var(--dimension-height);
+    }
+
+    :host([color='neutral']) {
+      color: var(--color-neutral-text);
+      background-color: var(--fhi-color-neutral-surface-default);
+    }
+
+    :host([color='accent']) {
+      --fhi-color: var(--fhi-color-accent-text-default);
+      --fhi-background-color: var(--fhi-color-accent-surface-default);
+    }
+
+    :host([color='success']) {
+      color: var(--fhi-color-success-text-default);
+      background-color: var(--fhi-color-success-surface-default);
+    }
+
+    :host([color='warning']) {
+      --fhi-color: var(--fhi-color-warning-text-default);
+      --fhi-background-color: var(--fhi-color-warning-surface-default);
+    }
+
+    :host([color='danger']) {
+      --fhi-color: var(--fhi-color-danger-text-default);
+      --fhi-background-color: var(--fhi-color-danger-surface-default);
+    }
+
+    :host([color='info']) {
+      --fhi-color: var(--fhi-color-info-text-default);
+      --fhi-background-color: var(--fhi-color-info-surface-default);
     }
   `;
 }
--- packages/fhi-designsystem/src/components/fhi-tag/fhi-tag.stories.ts
+++ packages/fhi-designsystem/src/components/fhi-tag/fhi-tag.stories.ts
@@ -1,16 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { FhiTag } from './fhi-tag.component';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { FhiIconRefresh } from '../icons/fhi-icon-refresh.component';
+import { FhiFlex } from '../fhi-flex/fhi-flex.component';
+import { FhiIconDownload } from '../icons/fhi-icon-download.component';
+import { FhiIconClock } from '../icons/fhi-icon-clock.component';
 
 new FhiTag();
+new FhiFlex();
+new FhiIconRefresh();
+new FhiIconDownload();
+new FhiIconClock();
 
 const meta: Meta<FhiTag> = {
   title: 'Komponenter/Tag',
   component: 'fhi-tag',
   parameters: {},
   decorators: [],
-  render: () => html`<fhi-tag>hi</fhi-tag>`,
-  argTypes: {},
+  render: args => html`<fhi-tag color=${ifDefined(args.color)}>Tag</fhi-tag>`,
+  argTypes: {
+    color: {
+      options: ['neutral', 'accent', 'success', 'warning', 'danger', 'info'],
+      control: { type: 'select' },
+      description: 'Bestemmer fargetema.',
+      defaultValue: { summary: 'neutral' },
+    },
+  },
 };
 
 type Story = StoryObj<FhiTag>;
@@ -18,6 +34,25 @@ type Story = StoryObj<FhiTag>;
 export const Preview: Story = {
   tags: [],
   args: {},
+};
+
+export const Icon: Story = {
+  tags: ['!dev'],
+  render: () =>
+    html`<fhi-flex direction="row" wrap>
+      <fhi-tag color="neutral"
+        ><fhi-icon-download></fhi-icon-download>Fra Kristin</fhi-tag
+      >
+      <fhi-tag color="accent"
+        ><fhi-icon-refresh></fhi-icon-refresh>Pågår</fhi-tag
+      >
+      <fhi-tag color="success">Publisert</fhi-tag>
+      <fhi-tag color="warning"
+        ><fhi-icon-clock></fhi-icon-clock>Utløper snart</fhi-tag
+      >
+      <fhi-tag color="danger">Ugyldig</fhi-tag>
+      <fhi-tag color="info">Offisiell statistikk</fhi-tag>
+    </fhi-flex>`,
 };
 
 export default meta;
