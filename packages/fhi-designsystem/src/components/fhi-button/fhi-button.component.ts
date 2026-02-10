413d702b63de861f0f8f9abd301e482946cbe6c9
Daniel Alexander Fermin Nilsen <dafn@outlook.com>
2026-02-10 15:18:32 +0100

    feat: make width 100% (#357)

--- packages/fhi-designsystem/src/components/fhi-button/fhi-button.component.ts
+++ packages/fhi-designsystem/src/components/fhi-button/fhi-button.component.ts
@@ -612,6 +612,8 @@ export class FhiButton extends LitElement {
         font-family: var(--typography-font-family);
         -webkit-font-smoothing: antialiased;
         transition: var(--motion-transition);
+        width: 100%;
+        justify-items: center;
 
         cursor: pointer;
         &:disabled {
