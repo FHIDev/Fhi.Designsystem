9f4ef62c92167e17b34dacca8cb1af4e4792955d
Daniel Nilsen <dafn@outlook.com>
2026-07-01 11:43:10 +0200

    add link component

---
+++ packages/fhi-designsystem/src/components/fhi-link/fhi-link.component.ts
@@ -0,0 +1,76 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+export const FhiLinkSelector = 'fhi-link';
+
+@customElement(FhiLinkSelector)
+export class FhiLink extends LitElement {
+  @property({ type: String, reflect: true })
+  href?: string;
+
+  @property({ type: String, reflect: true })
+  target?: '_self' | '_blank' | '_parent' | '_top' = '_self';
+
+  get rel() {
+    return this.target && this.target !== '_self'
+      ? 'noopener noreferrer'
+      : undefined;
+  }
+
+  render() {
+    return html`
+      <a
+        href="${ifDefined(this.href)}"
+        target="${ifDefined(this.target)}"
+        rel="${ifDefined(this.rel)}"
+      >
+        <slot></slot>
+      </a>
+    `;
+  }
+
+  static styles = css`
+    :host {
+      --fhi-link-color: unset;
+    }
+
+    :host {
+      --fhi-link-color: currentcolor;
+
+      letter-spacing: var(--fhi-typography-body-medium-letter-spacing);
+
+      font: var(--fhi-typography-body-medium-font-weight)
+        var(--fhi-typography-body-medium-font-size) /
+        var(--fhi-typography-body-medium-line-height)
+        var(--fhi-font-family-default);
+
+      a {
+        color: var(--fhi-link-color);
+        transition: color var(--fhi-motion-duration-quick)
+          var(--fhi-motion-ease-default);
+
+        text-decoration: underline;
+        text-underline-offset: 0.125rem;
+      }
+
+      &:host(:hover) {
+        --fhi-link-color: var(--fhi-color-accent-text-subtle);
+
+        a {
+          color: var(--fhi-color-accent-text-subtle);
+          text-decoration-thickness: 0.125rem;
+        }
+      }
+
+      &:host(:active) {
+        --fhi-link-color: currentcolor;
+
+        a {
+          color: var(--fhi-link-color);
+          text-underline-offset: 0.25rem;
+        }
+      }
+    }
+  `;
+}
---
+++ packages/fhi-designsystem/src/components/fhi-link/fhi-link.stories.ts
@@ -0,0 +1,74 @@
+import type { StoryObj } from '@storybook/web-components-vite';
+import { html } from 'lit';
+import { FhiLink } from './fhi-link.component';
+import { FhiBody } from '../typography/fhi-body/fhi-body.component';
+
+import { FhiStorybookMeta } from '../../../.storybook/fhi-meta';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+new FhiLink();
+new FhiBody();
+
+const meta: FhiStorybookMeta<FhiLink> = {
+  title: 'Komponenter/Link',
+  component: 'fhi-link',
+  parameters: {
+    eventTypes: [],
+    methodTypes: [],
+    slotTypes: [],
+  },
+  decorators: [],
+  render: args =>
+    html`<fhi-link
+      href="${ifDefined(args.href)}"
+      target="${ifDefined(args.target)}"
+      >Gå til FHI sin hjemmeside</fhi-link
+    >`,
+  argTypes: {
+    href: {
+      name: 'href',
+      description: 'URL for lenken.',
+      control: { type: 'text' },
+    },
+    target: {
+      name: 'target',
+      description:
+        'Angir hvor lenken skal åpnes. Se: [MDN Target Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/a#target) for mer informasjon',
+      control: { type: 'select' },
+      options: ['_self', '_blank', '_parent', '_top'],
+      defaultValue: { summary: '_self' },
+    },
+  },
+};
+
+type Story = StoryObj<FhiLink>;
+
+export const Preview: Story = {
+  tags: [],
+  args: {
+    href: 'https://www.fhi.no/',
+    target: '_blank',
+  },
+};
+
+export const Example: Story = {
+  tags: ['!dev'],
+  render: args =>
+    html`<fhi-body>
+      FHI Designsystem er et levende system under kontinuerlig, iterativ
+      utvikling. Kom gjerne med innspill, behov og tilbakemeldinger, for
+      eksempel gjennom
+      <fhi-link
+        href="${ifDefined(args.href)}"
+        target="${ifDefined(args.target)}"
+      >
+        våre skjema på Github
+      </fhi-link>
+    </fhi-body> `,
+  args: {
+    href: 'https://github.com/FHIDev/Fhi.Designsystem/issues',
+    target: '_blank',
+  },
+};
+
+export default meta;
